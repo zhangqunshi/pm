efd47e17cc2fbd7b989284cc0f6c228f199d2cee
zhangqunshi <zhangqunshi@126.com>
2017-05-30 13:08:04 +0800

    把写在代码中的关于数据库的连接信息放到database.properties配置文件中

--- src/com/nastation/pm/util/DBConn.java
+++ src/com/nastation/pm/util/DBConn.java
@@ -4,15 +4,38 @@ package com.nastation.pm.util;
  * @author 许希光
  * 作用: 数据库连接类
  */
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConn {
-	private static String className = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost:3306/projectmanagement?useUnicode=true&characterEncoding=UTF-8";
-	private static String user = "root";
-	private static String passwd = "123456";
+	private static String className;
+	private static String url;
+	private static String user;
+	private static String passwd;
+    
+        static {
+            Properties p = new Properties();
+            InputStream in = DBConn.class.getClassLoader().getResourceAsStream("database.properties");
+            try {
+                p.load(in);
+            } catch (IOException ex) {
+                Logger.getLogger(DBConn.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            className = p.getProperty("driver");
+            url = p.getProperty("url");
+            user = p.getProperty("user");
+            passwd = p.getProperty("password");
+            
+            System.out.println("DB className: " + className);
+            System.out.println("DB url: " + url);
+            System.out.println("DB user: " + user);
+        }
 
 	public static Connection getConnection() { // 获得数据库连接对象
 		Connection conn = null;
@@ -29,7 +52,9 @@ public class DBConn {
 
 	public static void closeConn(Connection conn) { // 关闭数据库对象
 		try {
+                    if (conn != null) {
 			conn.close();
+                    }
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
