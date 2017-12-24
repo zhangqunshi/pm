eb6beded1cdc4586c8d5df3fd896266560a0ff13
zhangqunshi <zhangqunshi@126.com>
2017-12-24 22:09:07 +0800

    Field isn't final but should be

--- src/com/nastation/pm/PermissionConstants.java
+++ src/com/nastation/pm/PermissionConstants.java
@@ -2,21 +2,21 @@ package com.nastation.pm;
 
 public class PermissionConstants {
 
-    public static String ADMINISTRATER_PROJECTS = "Administer Projects";
-    public static String BROWSE_PROJECTS = "Browse Projects";
-    public static String CREATE_ISSUES = "Create Issues";
-    public static String EDIT_ISSUES = "Edit Issues";
-    public static String SCHEDULE_ISSUES = "Schedule Issues";
-    public static String MOVE_ISSUES = "Move Issues";
-    public static String ASSIGN_ISSUES = "Assign Issues";
-    public static String ASSIGNABLE_USER = "Assignable User";
-    public static String RESOLVE_ISSUES = "Resolve Issues";
-    public static String CLOSE_ISSUES = "Close Issues";
-    public static String MODIFY_REPORTER = "Modify Reporter";
-    public static String DELETE_ISSUES = "Delete Issues";
-    public static String ADD_COMMENTS = "Add Comments";
-    public static String EDIT_ALL_COMMENTS = "Edit All Comments";
-    public static String EDIT_OWN_COMMENTS = "Edit Own Comments";
-    public static String DELETE_ALL_COMMENTS = "Delete All Comments";
-    public static String DELETE_OWN_COMMENTS = "Delete Own Comments";
+    public static final String ADMINISTRATER_PROJECTS = "Administer Projects";
+    public static final String BROWSE_PROJECTS = "Browse Projects";
+    public static final String CREATE_ISSUES = "Create Issues";
+    public static final String EDIT_ISSUES = "Edit Issues";
+    public static final String SCHEDULE_ISSUES = "Schedule Issues";
+    public static final String MOVE_ISSUES = "Move Issues";
+    public static final String ASSIGN_ISSUES = "Assign Issues";
+    public static final String ASSIGNABLE_USER = "Assignable User";
+    public static final String RESOLVE_ISSUES = "Resolve Issues";
+    public static final String CLOSE_ISSUES = "Close Issues";
+    public static final String MODIFY_REPORTER = "Modify Reporter";
+    public static final String DELETE_ISSUES = "Delete Issues";
+    public static final String ADD_COMMENTS = "Add Comments";
+    public static final String EDIT_ALL_COMMENTS = "Edit All Comments";
+    public static final String EDIT_OWN_COMMENTS = "Edit Own Comments";
+    public static final String DELETE_ALL_COMMENTS = "Delete All Comments";
+    public static final String DELETE_OWN_COMMENTS = "Delete Own Comments";
 }
